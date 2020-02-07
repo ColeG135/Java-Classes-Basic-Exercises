58b962b6d70ea24ffe4b0ded20bd9e80f812c08b
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-02-07 14:15:14 -0800

    Update rectangle.java

--- rectangle.java
+++ rectangle.java
@@ -1,1 +1,23 @@
+class Rectangle {
+    double width;
+    double height;
 
+    Rectangle() {
+        width = 10;
+        height = 10;
+    }
+
+    double area() {
+        return width * height;
+    }
+}
+
+public class Rectangle.java {
+    public static void main(String args[]) {
+        Rectangle mybox1 = new Rectangle();
+        double area;
+        area = mybox1.area();
+        System.out.println("Area is " + area);
+
+    }
+}
